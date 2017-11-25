d174e971e7d2cff732242350ccddcf13b9b92945
Roberto Antelo <roberto_antelo@hotmail.com>
2017-11-24 19:24:31 -0600

    work on test

--- test.js
+++ test.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const Morty = require('./index.js');
 
-
 //use OKTETA 
 fs.readFile('./multipart_in_base64.txt', (err, data) => {
   if (err) {
     throw err;
   }
 
-  console.log(data);
+  //Fucking boundary not working
+  let morty = new Morty(Buffer('----WebKitFormBoundarycknRrYwHG15UXgO3'));
+  let parsed = morty.parse(data)
+  console.log(parsed);
 });
