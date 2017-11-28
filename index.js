ddfe31cd083c2005f5ad8c3c00bbbd1cec4916f7
Roberto Antelo <roberto_antelo@hotmail.com>
2017-11-28 15:41:27 -0600

    Data can contain line breaks, allow extractBuffer to return without borderright

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Morty {
     if (!inBuffer(buff_chunk, border_left)) return '';
     let index_left = buff_chunk.indexOf(Buffer(border_left));
     let chunk = buff_chunk.slice(index_left + border_left.length);
-    return chunk.slice(0, chunk.indexOf(Buffer(border_right)));
+    return border_right ? chunk.slice(0, chunk.indexOf(Buffer(border_right))) : chunk;
   }
 
   parse(buff) {
@@ -26,13 +26,13 @@ class Morty {
       let response = {};
 
       response.name = this.extractFromBuffer(e, 'name="', '"').toString();
-      response.data = this.extractFromBuffer(e, '\r\n\r\n', '\r\n');
+      response.data = this.extractFromBuffer(e, '\r\n\r\n');
 
       if (this._hasFile(e)) {
         response.filename = this.extractFromBuffer(e, 'filename="', '"').toString();
         response.content_type = this.extractFromBuffer(e, 'Content-Type: ', '\r\n').toString();
       } else {
-        response.data = response.data.toString();
+        response.data = response.data.toString().trim();
       }
 
       return response;
